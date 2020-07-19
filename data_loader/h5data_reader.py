8df069b16ebd9e0c1db385f27f1149e6af93e7ac
Mongoal <1169666977@qq.com>
2020-07-19 11:21:53 +0800

    1.修复get_shuffle_data的bug 2.添加接口，可以直接设定读取指定条件的idx

--- data_loader/h5data_reader.py
+++ data_loader/h5data_reader.py
@@ -2,9 +2,10 @@
 # @Time    : 2020-03-29 22:04
 # @Author  : Lin Wanjie
 # @File    : h5data_reader.py
+import os
+
 import h5py
 import numpy as np
-import os
 
 
 # 打开h5 ，打乱，获取batch，
@@ -32,27 +33,40 @@ class H5DataReader(object):
         self.seed = seed
         self.current_id = 0
         np.random.seed(self.seed)
+        self.set_idx(train_set_ratio=train_set_ratio, seg_set_method=seg_set_method, txt=txt_path)
+
+    def __str__(self):
+        return str(self._file)
+
+    def set_idx(self, train_set_ratio=0.7, seg_set_method='shuffle', txt=None):
         self.shuffle_indices = np.random.permutation(self.length)
         if seg_set_method == 'continuous':
-            self.train_indices = np.random.permutation(int(self.length * train_set_ratio))
-            self.test_indices = np.arange(int(self.length * train_set_ratio), self.length)
+            self.train_indices = np.random.permutation(int(len(self.shuffle_indices)* train_set_ratio))
+            self.test_indices = np.arange(int(len(self.shuffle_indices) * train_set_ratio), self.length)
         elif seg_set_method == 'shuffle':
-            if (txt_path is not None and os.path.isfile(txt_path)):
-                self.shuffle_indices = np.loadtxt(txt_path, dtype=np.int32, delimiter='\n')
-            self.train_indices = self.shuffle_indices[:int(self.length * train_set_ratio)]
-            self.test_indices = self.shuffle_indices[int(self.length * train_set_ratio):]
+            if (txt is not None and os.path.isfile(txt)):
+                self.shuffle_indices = np.loadtxt(txt, dtype=np.int32, delimiter='\n')
+            self.train_indices = self.shuffle_indices[:int(len(self.shuffle_indices) * train_set_ratio)]
+            self.test_indices = self.shuffle_indices[int(len(self.shuffle_indices) * train_set_ratio):]
 
             np.savetxt('shuffle_idx.txt', self.shuffle_indices, '%d', '\n')
             np.savetxt('train_idx.txt', self.train_indices, '%d', '\n')
             np.savetxt('test_idx.txt', self.test_indices, '%d', '\n')
 
         elif seg_set_method == 'txt':
-            self.shuffle_indices = np.random.permutation(np.loadtxt(txt_path, dtype=np.int32, delimiter='\n'))
-            self.train_indices = self.shuffle_indices[:int(self.length * train_set_ratio)]
-            self.test_indices = self.shuffle_indices[int(self.length * train_set_ratio):]
+            self.shuffle_indices = np.random.permutation(np.loadtxt(txt, dtype=np.int32, delimiter='\n'))
+            self.train_indices = self.shuffle_indices[:int(len(self.shuffle_indices)* train_set_ratio)]
+            self.test_indices = self.shuffle_indices[int(len(self.shuffle_indices) * train_set_ratio):]
+        elif seg_set_method == 'array':
+            self.shuffle_indices = np.random.permutation(txt)
+            self.train_indices = self.shuffle_indices[:int(len(self.shuffle_indices)* train_set_ratio)]
+            self.test_indices = self.shuffle_indices[int(len(self.shuffle_indices) * train_set_ratio):]
 
-    def __str__(self):
-        return str(self._file)
+    def set_condition_idx(self, condition_keys: list = ['labels', 'fc'],
+                          include_conditions: list = None,
+                          exclude_conditions: list = None, outfile: str = None, shuffle=False, seg_ratio=None):
+        idx = self.get_condition_idx(condition_keys,include_conditions,exclude_conditions,outfile,shuffle)
+        self.set_idx(seg_ratio,seg_set_method='array',txt=idx)
 
 
     def get_condition_idx(self, condition_keys: list = ['labels', 'fc'],
@@ -146,7 +160,7 @@ class H5DataReader(object):
         labels_list = [self._labels[idx] for idx in indices]
         if (self.current_id + batch_size >= self.length):
             self.current_id = 0
-            self.shuffle_indices = np.random.permutation(self.length)
+            self.shuffle_indices = np.random.permutation(self.shuffle_indices)
         else:
             self.current_id += batch_size
         return data_list, labels_list
