0c1e1a2fe05cea0486ae93bdf75f6a3b9a981ba8
Mongoal <1169666977@qq.com>
2020-07-19 11:23:20 +0800

    finetune import 最大保存模型个数无法生效的尝试解决

--- load/ae_64x64_v5-ft_exp_2.0.py
+++ load/ae_64x64_v5-ft_exp_2.0.py
@@ -49,8 +49,8 @@ with tf.Graph().as_default():
         ckpt = tf.train.get_checkpoint_state(model_path).model_checkpoint_path
         # files = os.listdir(model_path)
         meta_file = ckpt+'.meta'
-        saver = tf.train.import_meta_graph(meta_file)
-        saver._max_to_keep=1
+        tf.train.import_meta_graph(meta_file)
+        saver = tf.train.Saver(max_to_keep=1)
 
         # Get input and output tensors
 
@@ -80,49 +80,64 @@ with tf.Graph().as_default():
         saver.restore(sess, ckpt)
 
         h5_reader = H5DataReader(stft_path,seg_set_method='txt',txt_path=train_idx_txt)
-
+        unknown_reader = H5DataReader(stft_path)
+        unknown_reader.set_condition_idx(condition_keys=['labels', 'fc'], include=[(6, 225), (7, 225), (8, 225)],exclude_conditions=None)
         i = 0
         los_list= []
-        e_los_list= []
         acc_list =[]
+        e_los_list= []
         e_acc_list =[]
+        u_los_list= []
+        u_acc_list =[]
         while(iters > i ):
             i+= 1
             batch_x, batch_y = h5_reader.get_train_batch(batch_size)
-            # preprocessing
             feed_dict = {is_training: False, input: batch_x, labels: batch_y}
             _, prd, los, acc =sess.run([train_op,predict,loss,accuracy],feed_dict)
             los_list.append(los)
             acc_list.append(acc)
-            batch_x, batch_y = h5_reader.get_train_batch(batch_size)
-            # preprocessing
+
+            batch_x, batch_y = h5_reader.get_test_batch(batch_size)
             feed_dict = {is_training: False, input: batch_x, labels: batch_y}
             e_prd, e_los, e_acc =sess.run([predict,loss,accuracy],feed_dict)
             e_los_list.append(los)
             e_acc_list.append(acc)
 
+            batch_x, batch_y = unknown_reader.get_shuffle_data(batch_size)
+            feed_dict = {is_training: False, input: batch_x, labels: batch_y}
+            u_prd, u_los, u_acc =sess.run([predict,loss,accuracy],feed_dict)
+            u_los_list.append(los)
+            u_acc_list.append(acc)
+
+
 
             if i%20 ==0:
                 los = np.mean(los_list)
-                e_los = np.mean(e_los_list)
                 acc = np.mean(acc_list)
+                e_los = np.mean(e_los_list)
                 e_acc = np.mean(e_acc_list)
+                u_los = np.mean(u_los_list)
+                u_acc = np.mean(u_acc_list)
                 los_list.clear()
-                e_los_list.clear()
                 acc_list.clear()
+                e_los_list.clear()
                 e_acc_list.clear()
-                print('step: [%d/%d]\tLoss %2.3f\tAcc %2.3f\tEval Loss %2.3f\tEval Acc %2.3f' %
-                  (i , iters,  los, acc, e_los, e_acc))
+                u_los_list.clear()
+                u_acc_list.clear()
+                print('step: [%d/%d]\tLoss %2.3f\tAcc %2.3f\tEval Loss %2.3f\tEval Acc %2.3f\tunknown Loss %2.3f\tunknown Acc %2.3f' %
+                  (i , iters,  los, acc, e_los, e_acc, u_los, u_acc))
 
                 summaries_dict = {
                     'los': los,
                     'acc': acc,
                     'e_los':e_los,
-                    'e_acc':e_acc
+                    'e_acc':e_acc,
+                    'u_los':u_los,
+                    'u_acc':u_acc,
                 }
                 logger.summarize(sess.run(finetune_step), summaries_dict=summaries_dict)
 
             if i%2000 == 0:
                 if not os.path.exists(finetune_model_dir):
                     os.mkdir(finetune_model_dir)
-                saver.save(sess, finetune_model_dir+'\ckpt',global_step=i)
+                saver.save(sess, finetune_model_dir,global_step=i)
